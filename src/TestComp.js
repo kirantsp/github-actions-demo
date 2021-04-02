226c5aa5f55bedf944c08478d10a19987bd7322d
kirantsp <31441411+kirantsp@users.noreply.github.com>
2021-04-02 16:25:28 +0530

    xss file

---
+++ src/TestComp.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+const Eval = () => {
+  const [data, setData] = useState({
+    firstName: '',
+    lastName: '',
+  });
+const handleType = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+const handleSubmit = () => {
+    eval(data.firstName + data.lastName);
+    //preventDefault();
+  };
+return (
+    <div>
+      <input
+        type='text'
+        name='firstName'
+        value={data.firstName}
+        onChange={(e) => handleType(e)}
+      />
+      <input
+        type='text'
+        name='lastName'
+        value={data.lastName}
+        onChange={(e) => handleType(e)}
+      />
+      <button onClick={() => handleSubmit()}>Submit</button>{' '}
+    </div>
+  );
+};
+export default Eval;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import TestComp from './TestComp';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TestComp />
   </React.StrictMode>,
   document.getElementById('root')
 );
