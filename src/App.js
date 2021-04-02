f36b6cc5ea1eb5341516cf80146f8aba0eaec6b4
kirantsp <31441411+kirantsp@users.noreply.github.com>
2021-04-02 10:40:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
+var i;
 function App() {
   return (
     <div className="App">
@@ -9,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-	Hello Phani Kiran!!
+	Hello Phani Kiran
         <a
           className="App-link"
           href="https://reactjs.org"
